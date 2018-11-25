6e65e1682672aa8391e4b05daa904b19a7224427
JarrianM <jarrian.mclean@gmail.com>
2018-11-25 01:11:50 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :school, :student, :roster
+def initialize(school)
+  @school = school
+  @roster = Hash.new{|roster,grade| roster[grade] = []}
+  # creates an empty array for the key within the @roster hash
+end
+
+def add_student(student,grade)
+@roster[grade] << student
+end
+
+def grade(grade)
+@roster[grade]
+end
+
+def sort
+@roster.each_value {|grade| grade.sort!}
+end
+end
